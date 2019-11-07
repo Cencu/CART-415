99ce22e32db6174f371281fd31d028d5b77e075e
Cencu <31831899+Cencu@users.noreply.github.com>
2019-11-06 23:17:07 -0500

    push

---
+++ 415 Test/Assets/Scripts/Interatable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interatable : MonoBehaviour
+{
+    // interactable items within the game that all share common values
+
+
+        //Radius in which the player needs to be within in order to interact with an object
+    public float radius = 3f;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+
+            }
+}
---
+++ 415 Test/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    Inventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // inventory = Inventory.instance;
+       // inventory.onItemChangedCallback += updateUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+
+    }
+}

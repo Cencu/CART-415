4dbf1f45842d03a06cde15f6de834722418623ef
Cencu <31831899+Cencu@users.noreply.github.com>
2019-11-13 17:35:58 -0500

    Push
    Fixed dialogue choices to appear and reappear

--- 415/Assets/Scripts/Dialogie/DialogueChoices.cs
+++ 415/Assets/Scripts/Dialogie/DialogueChoices.cs
@@ -5,22 +5,48 @@ using UnityEngine.UI;
 
 public class DialogueChoices : MonoBehaviour
 {
-    public GameObject DialogueBox;
+    public Text DialogueBox;
     public GameObject Choice01;
     public GameObject Choice02;
     public GameObject Choice03;
     public GameObject Choice04;
     public int ChoiceMade;
 
+
+    DialogueTrigger dialogueTrigger;
+
+    public void Start()
+    {
+        dialogueTrigger = GameObject.Find("EatStart").GetComponent<DialogueTrigger>();
+        Choice01 = GameObject.Find("Option01");
+    }
+
+    private void FixedUpdate()
+    {
+        if (dialogueTrigger.selections)
+        {
+            Choice01.SetActive(false);
+            Choice02.SetActive(false);
+        } else 
+        {
+            Choice01.SetActive(true);
+            Choice02.SetActive(true);
+        }
+
+    }
+
     public void ChoiceOption1()
     {
-        DialogueBox.GetComponent<Text>().text = "Have a good one.";
-        ChoiceMade = 1;
+        
+           // Choice01.GetComponent<Renderer>().enabled = false;
+            DialogueBox.text = "Have a good one.";
+            ChoiceMade = 1;
+        
     }
 
     public void ChoiceOption2()
     {
-        DialogueBox.GetComponent<Text>().text = "Bandits approach our village I hear. Best be hiding or getting away from here.";
+        DialogueBox.text = "Bandits approach our village I hear. Best be hiding or getting away from here.";
         ChoiceMade = 2;
     }
 
